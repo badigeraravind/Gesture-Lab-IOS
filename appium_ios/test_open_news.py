0a9f27213b31b807c80401421b9a22b39baa5a97
AravindBadiger <badigeraravinda@gmail.com>
2025-11-21 14:58:46 +0530

    Delete test_open_news.py

--- appium_ios/test_open_news.py
+++
@@ -1,6 +0,0 @@
-
-def test_open_news(driver):
-    driver.activate_app("com.apple.news")
-    info = driver.execute_script("mobile: activeAppInfo")
-    print("\nActive App: ", info)
-    assert info.get("bundleId") == "com.apple.news"
